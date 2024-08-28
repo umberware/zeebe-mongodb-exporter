70d8a8eab5692b2817bc322a1e85dcd2aee8feee
Hanor Sátiro Cintra <hanor.cintra@roit.com.br>
2024-08-28 16:30:47 -0300

    feat(last-event): created the las event information to save the last exported event of a key

--- src/main/java/io/zeebe/exporter/mongo/MongoExporterClient.java
+++ src/main/java/io/zeebe/exporter/mongo/MongoExporterClient.java
@@ -14,11 +14,16 @@ public class MongoExporterClient {
     updateOrInsert(String collection, String record) {
         Document parsedRecord = (Document.parse(record));
         Document event = new Document();
+
         event.append("timestamp", parsedRecord.get("timestamp"));
         event.append("intent", parsedRecord.get("intent"));
+        event.append("position", parsedRecord.get("position"));
+
+        parsedRecord.append("lastEvent", event);
 
         parsedRecord.remove("timestamp");
         parsedRecord.remove("intent");
+        parsedRecord.remove("position");
 
         Document filter = new Document("key", parsedRecord.get("key"));
         Document updateOperation = new Document("$set", parsedRecord);
